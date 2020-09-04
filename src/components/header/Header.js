b098c31e7bd0b710dfafee5ccd32353b549275b0
raccon-dev <loealoe@zohomail.eu>
2020-09-04 22:00:41 +0300

    added little animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -102,7 +102,7 @@ const Header = () => {
                 </nav>
 
                 <div className='help-section'>
-                    <p>Click Add button <br/> to create a new note!</p>
+                    <p>Click Add Button <br /> to create a new note!</p>
                     <img className='arrow-bottom' src='./assets/img/bottom-right-arrow.png' />
                 </div>
 
