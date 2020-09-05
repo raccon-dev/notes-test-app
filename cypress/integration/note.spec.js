7bfc5838c6899ad3938b7f55757a3f03c00d0659
raccon-dev <loealoe@zohomail.eu>
2020-09-05 10:02:50 +0300

    created spec for notes

---
+++ cypress/integration/note.spec.js
@@ -0,0 +1,44 @@
+describe('notes', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/');
+
+    });
+
+    it.only('Check title', () => {
+        cy.contains('Notes');
+    });
+    it.only('Check description', () => {
+        cy.contains('Take notes anywhere');
+    });
+
+    it.only('Russian Language', () => {
+        cy.get('.loc-RU').click()
+        cy.contains('Заметки');
+    });
+
+    it.only('English Language', () => {
+        cy.get('.loc-EN').click()
+        cy.contains('Notes');
+    });
+    it.only('Ukranian Language', () => {
+        cy.get('.loc-UA').click()
+        cy.contains('Нотатки');
+    });
+
+
+    it.only('Add new note', () => {
+        cy.get('.btn-addNotes').click()
+    });
+
+    it.only('Remove note', () => {
+        cy.get('.btn-removeNotes').first().click()
+    });
+
+    it.only('Edit note', () => {
+        cy.get('.btn-editNote').first().click()
+        cy.get('.notes-edit-section').first().type('U passed test! Congrat! ')
+        cy.get('.btn-editNote').first().click()
+    });
+
+
+});
